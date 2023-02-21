1239c1ebe9f68e77997210e94161f1fad6b655ed
AslinurTolga <aslinurtolga@gmail.com>
2023-02-22 01:38:06 +0300

    fix: background color changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Contact from "./routes/Contact";
+
 import Home from "./routes/Home";
 import Pricing from "./routes/Pricing";
 import Training from "./routes/Training";
