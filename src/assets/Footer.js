978b0f19fd0f567277acc0c9e76d572b8bb69d0a
AslinurTolga <aslinurtolga@gmail.com>
2023-02-21 22:09:54 +0300

    feat: footer is added

--- src/assets/Footer.js
+++ src/assets/Footer.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import "./FooterStyles.css";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+  FaSearchLocation,
+  FaTwitter,
+} from "react-icons";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaSearchLocation
+              size={20}
+              style={{ color: "#ffffff", marginRight: "2rem" }}
+            />
+            <div>
+              <p>123 Acme St.</p>
+              <h4>Houston, TX</h4>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              {" "}
+              <FaPhone
+                size={20}
+                style={{ color: "#ffffff", marginRight: "2rem" }}
+              />
+              1-800-123-1234
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#ffffff", marginRight: "2rem" }}
+              />
+              trips@space.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About the company</h4>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequuntur ipsam in eos esse suscipit quod dolor nesciunt quos?
+            Dolorem vel veniam iste reiciendis quia. Laudantium necessitatibus
+            beatae impedit sit placeat, maxime quisquam reiciendis, voluptate
+            accusantium corporis consequatur! Unde, reprehenderit quis veniam,
+            earum, sunt quisquam exercitationem temporibus maxime repellat magni
+            dignissimos."
+          </p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#ffffff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
